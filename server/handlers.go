3e213b10af7144f8d4a6b949463f4197091bb858
Alexander Polevoy <alexander.polevoy@oxagile.com>
2019-03-05 09:40:00 +0300

    feat(indexHandler): updated message to check deployments

--- server/handlers.go
+++ server/handlers.go
@@ -7,6 +7,6 @@ import (
 
 func (s *Server) indexHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from minicube app")
+		fmt.Fprintf(w, "Hello from minicube app - updated 05/03/2018")
 	}
 }
